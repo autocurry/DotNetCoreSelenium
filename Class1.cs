a19f09c4cb34a6910d9a5ca04e2c6f36ac6fbe7b
sandeep <sandeepsekhar555@gmail.com>
2018-11-11 14:22:56 +0800

    first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace DotNetCoreSelenium
+{
+    public class Class1{
+
+       IWebDriver _driver = new ChromeDriver("/home/sandeep/Desktop/DotNetCoreSelenium/");
+        [Fact]
+        public void FirstTest()
+        {
+            _driver.Navigate().GoToUrl("https://automationcurrypuff.home.blog");
+            Assert.True(true);
+        }
+    }
+}
